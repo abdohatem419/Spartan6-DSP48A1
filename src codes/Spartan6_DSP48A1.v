53d950dabbf2c829669997d2ebc622b8213cfbde
Abdelrahman Hatem Nasr <114186674+abdohatem419@users.noreply.github.com>
2024-07-29 08:57:25 +0300

    added new code version

--- src codes/Spartan6_DSP48A1.v
+++ src codes/Spartan6_DSP48A1.v
@@ -1,5 +1,20 @@
+//===========================================================
+// Project:     Spartan6_DSP48A1
+// File:        spartan6_DSP48A1.v
+// Author:      Abdelrahman Hatem Nasr
+// Date:        2024-07-28
+// Description: Implementation of DSP48A1 on Spartan 6 with
+//              various registers and parameters for signal
+//              processing. The module includes multipliers,
+//              adders, and control logic for data processing.
+//===========================================================
+
 module spartan6_DSP48A1(A,B,C,D,CARRYIN,M,P,CARRYOUT,CARRYOUTF,clk,OPMODE,CEA,CEB,CEC,CECARRYIN,CED,CEM,CEOPMODE,CEP,
 RSTA,RSTB,RSTC,RSTCARRYIN,RSTD,RSTM,RSTOPMODE,RSTP,BCOUT,BCIN,PCIN,PCOUT);
+
+//===========================================================
+// Parameter Declarations
+//===========================================================
 
 parameter A0REG       =0;
 parameter A1REG       =1;
@@ -16,6 +31,10 @@ parameter CARRYINSEL  ="OPMODE5";
 parameter B_INPUT     ="DIRECT";
 parameter RSTTYPE     ="SYNC";
 
+//===========================================================
+// ports Declarations
+//===========================================================
+
 input [17:0] A,B,D,BCIN;
 input [47:0] C,PCIN;
 input CARRYIN;
@@ -28,12 +47,40 @@ input CEA,CEB,CEC,CECARRYIN,CED,CEM,CEOPMODE,CEP;
 input RSTA,RSTB,RSTC,RSTCARRYIN,RSTD,RSTM,RSTOPMODE,RSTP;
 output [17:0] BCOUT;
 
+//===========================================================
+// Internal Signal Declarations
+// b_selected : input selected to reg_mux either B or BCIN
+// b_pre_adder_subtract : value of B to be added/subtracted with D
+// d_pre_adder_subtract : value of D to be added/subtracted with B
+// b_d_result_adder_subtract : result of pre adder/subtractor
+// pre_adder_subtract_selected : selected signal to B1REG either B value or adder/subtractor result
+// a_port : input signal to A1REG
+// multiplier_in_bd : input signal from B/D result to multiplier 
+// multiplier_in_a : input A signal to multiplier
+// multiplier_out : result of multiplier
+// m_mux_in : input signal to mux x from MREG
+// abd_concatenated : {D[11:0],A,B}
+// mux_x_selected : result of MUX X
+// mux_z_selected : result of MUX Z
+// post_adder_subtract_out : post adder result
+// c_port : Dedicated c port
+// carryin_selected : selected carry input either opmode5 or cascaded carry in
+// carryin_port : CIN input to post adder/subtractor
+// carryout_port : carry out result from post adder/subtractor 
+// opmode_reg : opmode reg_mux value
+// input_mux_zero : gnd port to muxes X and Z
+//===========================================================
+
 wire [17:0] b_selected,b_pre_adder_subtract,d_pre_adder_subtract,b_d_result_adder_subtract,pre_adder_subtract_selected,a_port,multiplier_in_bd,multiplier_in_a;
 wire [47:0] multiplier_out,m_mux_in;
 wire [47:0] abd_concatenated,mux_x_selected,mux_z_selected,post_adder_subtract_out,c_port;
 wire carryin_selected,carryin_port,carryout_port;
 wire [7:0] opmode_reg;
 wire [47:0]input_mux_zero;
+
+//===========================================================
+// Main functionality
+//===========================================================
 
 assign input_mux_zero=0;
 assign PCOUT=P;
@@ -55,6 +102,10 @@ Reg_Mux #(.PATH_WIDTH(18),.REG_ON(A1REG),.RSTTYPE(RSTTYPE)) ffm7 (.I0(a_port),.O
 assign BCOUT=multiplier_in_bd;
 assign multiplier_out=multiplier_in_a*multiplier_in_bd;
 Reg_Mux #(.PATH_WIDTH(48),.REG_ON(MREG),.RSTTYPE(RSTTYPE)) ffm8 (.I0(multiplier_out),.OUT(m_mux_in),.clk(clk),.rst(RSTM),.clk_enable(CEM));
+
+//===========================================================
+// buffer generation for multiplier result
+//===========================================================
 
 genvar i;
 generate
